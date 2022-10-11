7affc14ea25a4d6b31298789b87bab1f842df7a8
Conor Brett <conorbrett72@gmail.com>
2022-10-10 23:55:05 -0400

    Skybox added

--- Unity-Prototypes/Assets/Scripts/PlayerMovement.cs
+++ Unity-Prototypes/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
 
     public CharacterController controller;
 
-    public float speed = 20f;
+    public float speed = 10f;
     public float gravity = -100f;
 
     public Transform groundCheck;
@@ -68,10 +68,10 @@ public class PlayerMovement : MonoBehaviour
     //Sprinting
          if(Input.GetKey(KeyCode.LeftShift) && isGrounded)
         {
-            speed = 40f;
+            speed = 20f;
         }
         else{
-            speed = 20f;
+            speed = 10f;
         }
 
 
