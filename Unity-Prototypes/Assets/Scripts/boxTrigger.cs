e9c65407d709e4d76815190af1e52ab65a3d7263
Conor Brett <conorbrett72@gmail.com>
2023-02-24 20:44:53 +0000

    Started door teleporting script

--- Unity-Prototypes/Assets/Scripts/boxTrigger.cs
+++ Unity-Prototypes/Assets/Scripts/boxTrigger.cs
@@ -10,41 +10,63 @@ public class boxTrigger : MonoBehaviour
     public GameObject txtToDisplay;            //display the UI text
     
     private bool PlayerInZone;                  //check if the player is in box collider
+    private bool doorOpen;     
 
     int score; //Count number of items added to box
 
     [SerializeField] private Animator myAnimationController;
+    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private Transform player;
 
 
 
     //When scene starts - check if player is in the box coliider
+    
 
     private void Start()
     {
-
-
-        PlayerInZone = false;                   //player not in zone    
+        PlayerInZone = false;                   //player not in zone   
+        doorOpen = false;
         Debug.Log(txtToDisplay.GetComponent<UnityEngine.UI.Text>().text + " " + score); //Get the "Text" of the GameObject (This is getting the text of the UI element)
         txtToDisplay.SetActive(false);
+
+
     }
+
+    
 
     private void Update()
     {
+
+        //If player enters Door radius
         if (PlayerInZone && Input.GetKeyDown(KeyCode.F))           //if in zone and press F key
         {
-
-            
-            gameObject.GetComponent<AudioSource>().Play();
-            gameObject.GetComponent<Animator>().Play("switch");
-           
+           //player.transform.position = new Vector3(respawnPoint.transform.position);
+           //player.transform.position = new Vector3(-130f, -88f, 20f);
+           //Debug.Log(respawnPoint.transform.position);
+           Debug.Log("Enter Door");
+           doorOpen = true;
         }
     }
 
 
-    //Display text if Mushroom is added is in box collider
 
+    //Display text if Mushroom is added is in box collider
     private void OnTriggerEnter(Collider other)
     {
+
+    //Display text If player enters Door radius
+
+        if (other.gameObject.tag == "Player" && gameObject.tag == "Door")     //if player is inside Door box collider
+        {
+            txtToDisplay.SetActive(true);
+            PlayerInZone = true;            
+            player.transform.position = new Vector3(-130f, -88f, 20f);
+            
+        }
+
+
+
 
         //if Mushroom is placed in MushroomCounter box collider
         if (other.gameObject.tag == "Mushroom" && gameObject.tag == "MushroomCounter")     
@@ -72,9 +94,23 @@ public class boxTrigger : MonoBehaviour
 
      }
     
-    //if Mushroom is removed from MushroomCounter box collider
     private void OnTriggerExit(Collider other)    
     {
+
+
+
+ //Remove text if player leaves Door radius
+
+        if (other.gameObject.tag == "Player" && gameObject.tag == "Door")     //if player is inside Door box collider
+        {
+            txtToDisplay.SetActive(false);
+            PlayerInZone = false;
+        }
+
+
+
+
+    //if Mushroom is removed from MushroomCounter box collider
         if (other.gameObject.tag == "Mushroom" && gameObject.tag == "MushroomCounter")
         {
             txtToDisplay.SetActive(true);
