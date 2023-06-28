d3556c846f17cec727e0ede123a6992d89206479
Conor Brett <conorbrett72@gmail.com>
2023-06-28 01:42:24 +0100

    Basic NPC interaction added
    Very basic NPC interaction. Player can press E on an NPC when within 2f of the NPC collider. This returns a console log atm.

---
+++ 3D 3rd Person Movement/Assets/Scripts/NPCInteractable.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteractable : MonoBehaviour, IInteractable {
+
+    [SerializeField] private string interactText;
+
+    private Animator animator;
+  //  private NPCHeadLookAt npcHeadLookAt;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+     //   npcHeadLookAt = GetComponent<NPCHeadLookAt>();
+    }
+
+    public void Interact(Transform interactorTransform) {
+     // ChatBubble3D.Create(transform.transform, new Vector3(-.3f, 1.7f, 0f), ChatBubble3D.IconType.Happy, "Hello there!");
+       Debug.Log("Test");
+
+        //animator.SetTrigger("Talk");
+
+        float playerHeight = 1.7f;
+     //   npcHeadLookAt.LookAtPosition(interactorTransform.position + Vector3.up * playerHeight);
+    }
+
+    public string GetInteractText() {
+        return interactText;
+    }
+
+    public Transform GetTransform() {
+        return transform;
+    }
+
+}
---
+++ 3D 3rd Person Movement/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour {
+
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            IInteractable interactable = GetInteractableObject();
+            if (interactable != null) {
+                interactable.Interact(transform);
+            }
+        }
+    }
+
+    public IInteractable GetInteractableObject() {
+        List<IInteractable> interactableList = new List<IInteractable>();
+        float interactRange = 3f;
+        Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+        foreach (Collider collider in colliderArray) {
+            if (collider.TryGetComponent(out IInteractable interactable)) {
+                interactableList.Add(interactable);
+            }
+        }
+
+        IInteractable closestInteractable = null;
+        foreach (IInteractable interactable in interactableList) {
+            if (closestInteractable == null) {
+                closestInteractable = interactable;
+            } else {
+                if (Vector3.Distance(transform.position, interactable.GetTransform().position) < 
+                    Vector3.Distance(transform.position, closestInteractable.GetTransform().position)) {
+                    // Closer
+                    closestInteractable = interactable;
+                }
+            }
+        }
+
+        return closestInteractable;
+    }
+
+}
